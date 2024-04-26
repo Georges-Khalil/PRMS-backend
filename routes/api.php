6f12c4a3c2e77873f3396385f97ccfb97ebc1441
Georges Khalil <georgesghassankhalil@gmail.com>
2024-04-26 14:36:53 +0300

    added api endpoint for getting the reports

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\ReportsController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,4 @@ Route::get('/user', function (Request $request) {
 Route::post('/login', [UsersController::class, 'login']);
 Route::post('/register', [UsersController::class, 'register']);
 Route::get('/user/projects', [ProjectsController::class, 'getProjects']);
+Route::get('/projects/{projectId}/reports', [ReportsController::class, 'getReportsByProject']);
